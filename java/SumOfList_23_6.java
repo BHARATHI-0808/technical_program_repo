add9c42a84d87c078f9adcdca1d1357ab7616e0e
bharathi M <bharathimr08@gmail.com>
2024-06-23 19:08:13 +0530

    today problem solving

---
+++ java/SumOfList_23_6.java
@@ -0,0 +1,13 @@
+public class SumOfList_23_6 {
+	static int SumOfList(int[] a) {
+		int sum=0;
+		for(int i=0;i<a.length;i++) {
+			sum+=a[i];
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		int[] num= {0,3,4};
+		System.out.println("sum of list = "+ SumOfList(num));
+	}
+}
---
+++ python/evenNumber_23_6.py
@@ -0,0 +1,6 @@
+def PrintEvenNumber(a):
+    for i in a:
+        if i%2==0:
+            print(i)
+number = [1,2,3,4,5,6]
+PrintEvenNumber(number)
